db8e5793be627b32c6d473586914175cd03cc1c8
Evert van Groningen <evertvangroningen@gmail.com>
2026-09-03 14:38:36 +0200

    gameFinished

--- GameJam/Assets/Scripts/SceneChanger.cs
+++ GameJam/Assets/Scripts/SceneChanger.cs
@@ -3,13 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] GameObject RulesPanel;
     // Update is called once per frame
     void Update()
     {
         string sceneName = SceneManager.GetActiveScene().name;
         if ((sceneName == "P1Win" || sceneName == "P2Win") && Input.anyKeyDown)
         {
-            SceneManager.LoadScene("Game");
+            SceneManager.LoadScene("MainMenu");
         }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Rules()
+    {
+        RulesPanel.SetActive(true);
+    }
+
+    public void CloseRules()
+    {
+        RulesPanel.SetActive(false);
     }
 }
